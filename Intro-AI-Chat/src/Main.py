22577c4f7cccf9f8fb7fe20b93151dd3688b4284
sibulelozondi <zondi.sibulelo@gmail.com>
2026-07-02 18:42:22 +0200

    Update Main.py

--- Intro-AI-Chat/src/Main.py
+++ Intro-AI-Chat/src/Main.py
@@ -1,12 +1,12 @@
 
 
-##API_KEY = ""
+API_KEY = ""
 
 from langchain.chat_models import init_chat_model
 
 model = init_chat_model(model="gemini-3-flash-preview"
                         ,model_provider="google-genai"
-                        ,api_key="")##API_KEY)
+                        ,api_key=API_KEY)
 
 
 with open('data.txt') as file:
